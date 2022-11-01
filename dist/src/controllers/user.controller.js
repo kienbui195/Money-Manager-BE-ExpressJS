cd0790c91d2edbe203d7149e273273f15e6b5286
Kay Bi <kaybi9x@gmail.com>
2022-11-01 16:17:03 +0700

    update api

--- dist/src/controllers/user.controller.js
+++ dist/src/controllers/user.controller.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -31,126 +8,93 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-<<<<<<< HEAD
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const user_model_1 = require("../schemas/user.model");
-const bcrypt = __importStar(require("bcrypt"));
 const console_1 = __importDefault(require("console"));
 const mail_setup_1 = __importDefault(require("../tools/Verify Email/mail.setup"));
-=======
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_model_1 = require("../schemas/user.model");
-const bcrypt = __importStar(require("bcrypt"));
->>>>>>> ecafb75a09c5292ef2b151fc419a1d058141107f
 class User {
     constructor() {
         this.getAllUser = (req, res) => __awaiter(this, void 0, void 0, function* () {
             const user = yield user_model_1.UserModel.find();
             try {
-                res.status(200).json(user);
+                res.status(200).json({ type: 'success', message: user });
             }
             catch (err) {
-                res.status(200).json({ message: err });
+                res.status(200).json({ type: 'error', message: err });
             }
         });
         this.register = (req, res) => __awaiter(this, void 0, void 0, function* () {
             try {
                 let user = req.body;
-<<<<<<< HEAD
                 let userId = yield user_model_1.UserModel.findOne({ email: req.body.email });
                 if (userId == null) {
-                    user.password = yield bcrypt.hash(user.password, 10);
                     let newUser = yield user_model_1.UserModel.create(user);
                     const newID = newUser.id;
                     (0, mail_setup_1.default)(req, res, newID);
-                    res.status(201).json({ userId: newUser._id, message: "Register Successfully" });
+                    res.status(201).json({ type: 'success', message: "Register Successfully" });
                 }
                 else {
                     res.status(200).json({
-                        err: "User already exists"
-=======
-                let checkUser = yield user_model_1.UserModel.findOne({ email: user.email });
-                if (!checkUser) {
-                    user.password = yield bcrypt.hash(user.password, 10);
-                    user = yield user_model_1.UserModel.create(user);
-                    res.status(201).json({ type: 'success', message: 'Lưu điểm thành công!' });
-                }
-                else {
-                    res.status(200).json({
-                        err: "User exited"
->>>>>>> ecafb75a09c5292ef2b151fc419a1d058141107f
+                        type: 'exist',
+                        message: "User already exists"
                     });
                 }
             }
             catch (error) {
-<<<<<<< HEAD
                 console_1.default.log(error);
                 res.status(500).json('Server error');
-=======
->>>>>>> ecafb75a09c5292ef2b151fc419a1d058141107f
             }
         });
         this.getUserById = (req, res) => __awaiter(this, void 0, void 0, function* () {
             const userId = req.params.id;
             const user = yield user_model_1.UserModel.findById({ _id: userId }, req.body);
             try {
-                res.status(200).json(user);
+                res.status(200).json({ type: 'success', message: user });
             }
             catch (err) {
-                res.status(200).json({ message: err });
+                res.status(200).json({ type: 'error', message: err });
             }
         });
         this.updateUser = (req, res) => __awaiter(this, void 0, void 0, function* () {
-<<<<<<< HEAD
             console_1.default.log(req.body);
-=======
-            console.log(req.body);
->>>>>>> ecafb75a09c5292ef2b151fc419a1d058141107f
             let id = req.params.id;
             let publisher = yield user_model_1.UserModel.findById(id);
             if (!publisher) {
-                res.status(404).json();
+                res.status(200).json({ type: 'notexist', message: "Update user fail!!!" });
             }
             else {
                 let data = req.body;
                 let newUser = yield user_model_1.UserModel.findByIdAndUpdate({ _id: id }, data);
-                res.status(200).json(newUser);
+                res.status(200).json({ type: 'success', message: newUser });
             }
         });
         this.deleteUser = (req, res) => __awaiter(this, void 0, void 0, function* () {
             let id = req.params.id;
             let user = yield user_model_1.UserModel.findById(id);
             if (!user) {
-                res.status(404).json({ message: "No User Delete" });
+                res.status(200).json({ type: 'notexist', message: "No User Delete" });
             }
             user === null || user === void 0 ? void 0 : user.delete();
-            res.status(204).json();
+            res.status(200).json({ type: 'success', message: 'Delete successfully!' });
         });
-<<<<<<< HEAD
         this.postVerifyUser = (req, res) => __awaiter(this, void 0, void 0, function* () {
             let id = req.params.id;
             try {
                 let idUser = yield user_model_1.UserModel.findByIdAndUpdate({ _id: id }, { isVerify: true });
                 if (idUser) {
-                    res.status(200).json({ message: "Verify successfully" });
+                    res.status(200).json({ type: 'success', message: "Verify successfully" });
+                }
+                else {
+                    res.status(200).json({ type: 'error', message: "Error Verify" });
                 }
             }
             catch (error) {
-                console_1.default.log(error);
-                res.status(404).json({ error: error });
+                res.status(200).json({ type: 'error', message: error });
             }
         });
     }
 }
 exports.default = new User();
-=======
-        this.verifyUser = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            let id = req.params.id;
-            let idUser = yield user_model_1.UserModel.findByIdAndUpdate({ _id: id }, { isVerify: true });
-        });
-    }
-}
->>>>>>> ecafb75a09c5292ef2b151fc419a1d058141107f
--- dist/src/routers/user.route.js
+++ dist/src/routers/user.route.js
@@ -1,5 +1,4 @@
 "use strict";
-<<<<<<< HEAD
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -10,15 +9,3 @@ const user_controller_1 = __importDefault(require("../controllers/user.controlle
 exports.userRouter = (0, express_1.Router)();
 exports.userRouter.post('/register', user_controller_1.default.register);
 exports.userRouter.post('/verify/:id', user_controller_1.default.postVerifyUser);
-=======
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-const mongoose_1 = require("mongoose");
-const userSchema = new mongoose_1.Schema({
-    username: String,
-    email: String,
-    password: String
-});
-const User = (0, mongoose_1.model)('User', userSchema);
-exports.User = User;
->>>>>>> ecafb75a09c5292ef2b151fc419a1d058141107f
