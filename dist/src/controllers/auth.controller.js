22c36a8db4bf8a3b0da3856546b041196f81e8cc
Kay Bi <kaybi9x@gmail.com>
2022-11-01 21:57:56 +0700

    update api

--- dist/src/controllers/auth.controller.js
+++ dist/src/controllers/auth.controller.js
@@ -39,56 +39,62 @@ class AuthController {
                 res.status(500).json('Server error');
             }
         });
-        this.postVerifyUser = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            let id = req.params.id;
+        this.verifyUser = (req, res) => __awaiter(this, void 0, void 0, function* () {
             try {
-                let idUser = yield UserModel.findByIdAndUpdate({ _id: id }, { isVerify: true });
-                if (idUser) {
-                    res.status(200).json({ type: 'success', message: "Verify successfully" });
+                let id = req.params.id;
+                const user = yield UserModel.findOne({ _id: id });
+                if (!user) {
+                    res.status(200).json({ type: 'notexist', message: 'Verify Fail' });
                 }
                 else {
-                    res.status(200).json({ type: 'error', message: "Error Verify" });
+                    yield UserModel.findOneAndUpdate({ _id: id }, { isVerify: true });
+                    res.status(200).json({ type: 'success', message: 'Verify Success' });
                 }
             }
             catch (error) {
-                res.status(200).json({ type: 'error', message: error });
+                console.log(error);
             }
         });
     }
     postLogin(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            const data = req.body;
-            const user = yield UserModel.findOne({ email: data.email });
-            if (user) {
-                if (data.password === user.password) {
-                    let payload = {
-                        user_id: user["id"],
-                        email: user["email"]
-                    };
-                    const token = jsonwebtoken_1.default.sign(payload, '230193', {
-                        expiresIn: 36000,
-                    });
-                    res.status(200)
-                        .cookie('jwt_token', JSON.stringify(token), {
-                        httpOnly: true,
-                        maxAge: 1 * 15 * 1 * 1
-                    })
-                        .json({
-                        type: 'success', message: {
-                            message: 'Signed in successfully!',
-                            data: user
-                        }
-                    });
+            try {
+                const data = req.body;
+                const user = yield UserModel.findOne({ email: data.email });
+                if (user) {
+                    if (data.password === user.password) {
+                        let payload = {
+                            user_id: user["id"],
+                            email: user["email"]
+                        };
+                        const token = jsonwebtoken_1.default.sign(payload, '230193', {
+                            expiresIn: 36000,
+                        });
+                        res.status(200)
+                            .cookie('jwt_token', JSON.stringify(token), {
+                            httpOnly: true,
+                            maxAge: 1 * 15 * 1 * 1
+                        })
+                            .json({
+                            type: 'success', message: {
+                                message: 'Signed in successfully!',
+                                data: user
+                            }
+                        });
+                    }
+                    else {
+                        res.status(200).json({ type: 'error', message: 'Password is not correct!' });
+                    }
                 }
                 else {
-                    res.status(200).json({ type: 'error', message: 'Password is not correct!' });
+                    res.status(200).json({
+                        type: 'error',
+                        message: 'Account does not exist yet!',
+                    });
                 }
             }
-            else {
-                res.status(200).json({
-                    type: 'error',
-                    message: 'Account does not exist yet!',
-                });
+            catch (err) {
+                res.status(500).json('Server error');
             }
         });
     }
