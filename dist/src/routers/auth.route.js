dbccf289f27a6a066001c09f2626cba7514de1e3
Kay Bi <kaybi9x@gmail.com>
2022-11-01 14:00:39 +0700

    update code

--- dist/src/routers/auth.route.js
+++ dist/src/routers/auth.route.js
@@ -4,13 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const AuthController = require('../controllers/auth.controller');
+const login_middleware_1 = require("../middleware/login.middleware");
+const auth_controller_1 = __importDefault(require("../controllers/auth.controller"));
 const authRouter = express_1.default.Router();
-const authController = new AuthController();
 authRouter.post('/register', (req, res) => {
-    authController.register(req, res).catch(() => { res.status(500).json('Server error'); });
+    auth_controller_1.default.register(req, res).catch(() => { res.status(500).json('Server error'); });
 });
 authRouter.post('/login', (req, res) => {
-    authController.postLogin(req, res).catch(() => res.status(500).json('server error'));
-});
+    auth_controller_1.default.postLogin(req, res).catch(() => res.status(500).json('server error'));
+}, login_middleware_1.auth);
 exports.default = authRouter;
