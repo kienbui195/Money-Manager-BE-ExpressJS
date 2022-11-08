58f245ab0de0c5cb034cf0d9fb82a0a9e610c6e3
ProDuck993 <produck.1993@gmail.com>
2022-11-08 14:36:16 +0700

    crud categories

--- src/routers/category.route.ts
+++ src/routers/category.route.ts
@@ -5,6 +5,7 @@ const categoryRoute = express.Router();
 
 categoryRoute.post('/add-category', categoryController.createCategory);
 categoryRoute.get('/get-category/:id', categoryController.getAllCategory);
+categoryRoute.get('/get-category-byuser/:id', categoryController.getCategoryByIdUser);
 categoryRoute.put('/update-categody/:id', categoryController.postUpdateCategory);
 categoryRoute.delete('/delete-category/:id', categoryController.deleteCategory);
 
--- src/routers/wallet.route.ts
+++ src/routers/wallet.route.ts
@@ -4,6 +4,7 @@ import walletController from "../controllers/wallet.controller";
 const walletRoute = express.Router()
 
 walletRoute.get('/get-all-wallet/:id', walletController.getWalletByIdUser);
+walletRoute.get('/get-all-wallet/:id', walletController.getWalletByIdUser);
 walletRoute.get('/total/:id', walletController.getTotalMoney);
 walletRoute.post('/create', walletController.createWallet);
 walletRoute.put('/update/:id', walletController.updateWallet);
