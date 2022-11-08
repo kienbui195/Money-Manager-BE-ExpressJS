afdf22466c67cf1a6d0f484b6a57aa28a76aff62
ProDuck993 <produck.1993@gmail.com>
2022-11-08 10:54:16 +0700

    crud category

--- src/routers/category.route.ts
+++ src/routers/category.route.ts
@@ -5,5 +5,7 @@ const categoryRoute = express.Router();
 
 categoryRoute.post('/add-category', categoryController.createCategory);
 categoryRoute.get('/get-category/:id', categoryController.getAllCategory);
+categoryRoute.put('/update-categody/:id', categoryController.getAllCategory);
+categoryRoute.delete('/delete-category/:id', categoryController.getAllCategory);
 
 export default categoryRoute;
