e2ffaec72e4dfcb7aa2115f29b8533bbfb853128
Kay Bi <kaybi9x@gmail.com>
2022-11-10 14:37:58 +0700

    fix bug

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
 import { UserModel } from "../schemas/user.model";
 import { WalletModel } from "../schemas/wallet.schema";
-import {TransactionModel} from "../schemas/transaction.schema";
+import { TransactionModel } from "../schemas/transaction.schema";
 import getFormatDate from "../tools/formatDate";
 class WalletController {
 
 
     async getWalletByIdUser(req: Request, res: Response) {
-        let id = req.params.id;
-        const wallet = await WalletModel.find({user_id :  id })
+
         try {
+            let id = req.params.id;
+            const wallet = await WalletModel.find({ user_id: id })
             res.status(200).json({ type: 'success', wallet })
         } catch (err) {
             res.status(500).json('Server error')
@@ -17,18 +18,19 @@ class WalletController {
     }
 
     async createWallet(req: Request, res: Response) {
-        const data = req.body
-        const wallet = new WalletModel({
-            icon : data.icon,
-            name : data.name,
-            user_id: data.user_id,
-            amount : data.amount
-        })
-        let allWallet = await WalletModel.findOne({ name: wallet.name })
+
         try {
+            const data = req.body
+            const wallet = new WalletModel({
+                icon: data.icon,
+                name: data.name,
+                user_id: data.user_id,
+                amount: data.amount
+            })
+            let allWallet = await WalletModel.findOne({ name: wallet.name })
             if (!allWallet) {
-               await wallet.save()
-                let Wallet = await WalletModel.findOne({ name: wallet.name, user_id: data.user_id})
+                await wallet.save()
+                let Wallet = await WalletModel.findOne({ name: wallet.name, user_id: data.user_id })
                 let today = new Date();
                 let dateNow = getFormatDate(today)
                 if (Wallet && wallet.amount > 0) {
@@ -43,7 +45,7 @@ class WalletController {
                         wallet_name: wallet.name,
                         wallet_icon: wallet.icon,
                         user_id: data.user_id,
-                        note:'',
+                        note: '',
                     }
                     await TransactionModel.create(transaction)
                 }
@@ -62,22 +64,23 @@ class WalletController {
     }
 
     async updateWallet(req: Request, res: Response) {
-        const wallet = req.body;
-        let idWallet = req.params.id;
-        let walletFind = await WalletModel.findOne({ _id: idWallet })
+
         try {
+            const wallet = req.body;
+            let idWallet = req.params.id;
+            let walletFind = await WalletModel.findOne({ _id: idWallet })
             if (walletFind) {
                 await WalletModel.findOneAndUpdate({ _id: idWallet }, wallet)
-                const newWallet = await WalletModel.findOne({_id : idWallet})
-                if(walletFind.amount !== wallet.amount) {
+                const newWallet = await WalletModel.findOne({ _id: idWallet })
+                if (walletFind.amount !== wallet.amount) {
                     let name;
                     let type;
                     let amount;
-                    if(walletFind.amount < wallet.amount) {
+                    if (walletFind.amount < wallet.amount) {
                         name = 'Other Income'
                         type = 'income'
                         amount = wallet.amount - walletFind.amount
-                    }else if(walletFind.amount > wallet.amount) {
+                    } else if (walletFind.amount > wallet.amount) {
                         name = 'Other Expense'
                         type = 'expense'
                         amount = walletFind.amount - wallet.amount
@@ -95,7 +98,7 @@ class WalletController {
                         wallet_name: wallet.name,
                         wallet_icon: wallet.icon,
                         user_id: walletFind.user_id,
-                        note:'',
+                        note: '',
                     }
                     await TransactionModel.create(transaction)
                 }
@@ -110,26 +113,28 @@ class WalletController {
     }
 
     async deleteWallet(req: Request, res: Response) {
-        let id = req.params.id
+
         try {
+            let id = req.params.id
             let wallet = await WalletModel.findById(id);
             if (!wallet) {
                 res.status(200).json({ type: 'notexist', message: "No Wallet Delete" });
             } else {
                 wallet?.delete();
-                await TransactionModel.deleteMany({wallet_id : id})
+                await TransactionModel.deleteMany({ wallet_id: id })
                 res.status(200).json({ type: 'success', message: 'Delete successfully!' });
             }
         } catch (err) {
             res.status(500).json('Server error')
-        }  
+        }
     }
 
-    async getTotalMoney (req: Request, res: Response) {
-        let id = req.params.id;
-        const findWalletByUser = await WalletModel.find({user_id :  id })
+    async getTotalMoney(req: Request, res: Response) {
+
         try {
-           const total = findWalletByUser.reduce((total,item) => total = total +item.amount, 0 )
+            let id = req.params.id;
+            const findWalletByUser = await WalletModel.find({ user_id: id })
+            const total = findWalletByUser.reduce((total, item) => total = total + item.amount, 0)
             res.status(200).json({ type: 'success', total })
         } catch (err) {
             res.status(500).json('Server error')
