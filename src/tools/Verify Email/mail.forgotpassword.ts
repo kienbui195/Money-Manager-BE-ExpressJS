242516cfd4c7369eb2b79746db0a690057c93ab0
Kien Bui <kaybi9x@gmail.com>
2023-03-31 09:53:23 +0700

    sua loi dang nhap

--- src/tools/Verify Email/mail.forgotpassword.ts
+++ src/tools/Verify Email/mail.forgotpassword.ts
@@ -26,7 +26,7 @@ const forgotPasswordByEmail = (req: Request, res: Response, id: string) => {
         <h1 style="color: #ee1414; width: 100%; text-align: center; font-size: 20px;">Change password</h1>
 
         <div style="color: black; font-size: 35px; width: 100%; text-align: center; height: 50px;">
-        <h3 style="color: #ee1414; width: 100%; text-align: center; font-size: 20px;">Click <a href="https://localhost:3001/user/forgotPassword/${id}">here</a> to verify!</h3>
+        <h3 style="color: #ee1414; width: 100%; text-align: center; font-size: 20px;">Click <a href="http://localhost:3001/user/forgotPassword/${id}">here</a> to verify!</h3>
         </div>
     </div>
     </div>
